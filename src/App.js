c1031aea83e5ded0205e3331d1feb9f9b35373c2
James Shin <jhscode@gmail.com>
2018-09-24 19:10:14 -0400

    add api key and api call with beer as description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+const apiKey = process.env.REACT_APP_API_KEY;
+const rootURL = `http://lcboapi.com/products?access_key=${apiKey}`
+
+class App extends Component {  
+  state = {
+    product: 'beer',
+    data: null
+  }
+  
+  onClickBeer = async (e) => {
+    const url = await fetch(`${rootURL}&q=beer`);
+    const data = await url.json();
+    console.log(data);
+    this.setState({
+      data: data.results
+    })
+  }
+  
   render() {
     return (
       <div className="App">
+      <button onClick={ this.onClickBeer }>Click Me!</button>
       </div>
     );
   }
