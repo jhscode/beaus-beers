22d0aa84c6e2b60bb4cbfa7419b1f197b8b31d35
James Shin <jhscode@gmail.com>
2019-01-10 14:05:11 -0500

    Updates

---
+++ precache-manifest.7a64f0729d0989f11bcc5bb52312e002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42f83a1e8c029134c68d",
+    "url": "/beaus-beers/static/js/runtime~main.42f83a1e.js"
+  },
+  {
+    "revision": "f4eca41aacd2ce3c9edf",
+    "url": "/beaus-beers/static/js/main.f4eca41a.chunk.js"
+  },
+  {
+    "revision": "0ebbf6cd16bcff9e2fb9",
+    "url": "/beaus-beers/static/js/1.0ebbf6cd.chunk.js"
+  },
+  {
+    "revision": "f4eca41aacd2ce3c9edf",
+    "url": "/beaus-beers/static/css/main.f213eb7d.chunk.css"
+  },
+  {
+    "revision": "f7dfb4da163c02487d6e5d0e6ef72d4d",
+    "url": "/beaus-beers/index.html"
+  }
+];
